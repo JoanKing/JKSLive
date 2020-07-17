e9eed2fb871231a6c41457efd7eceb845b806b19
WangChong <wangchong@cloud-young.com>
2020-07-17 14:43:19 +0800

    初始化项目

---
+++ JKSLive/JKSLive/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JKSLive
+//
+//  Created by 王冲 on 2020/7/17.
+//  Copyright © 2020 王冲. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ JKSLive/JKSLive/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  JKSLive
+//
+//  Created by 王冲 on 2020/7/17.
+//  Copyright © 2020 王冲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .green
+        
+    }
+
+
+}
+
